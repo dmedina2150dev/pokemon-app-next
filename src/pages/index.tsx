0c7762d793d97961f0cfda89bfb28a4c120f7b7b
Dajan Medina <dmedina2150dev@gmail.com>
2023-04-28 22:12:32 -0400

    [Add] Configuraciones para cargar los 151 pokemons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { NextPage, GetStaticProps } from 'next';
 
 import { Layout } from '@/components/layouts';
+import { pokeApi } from '@/api';
 
 /**
  * 
@@ -36,10 +37,13 @@ const HomePage: NextPage = (props) => {
 
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
+
+  const { data } = await pokeApi.get('/pokemon?limit=151');
+  // console.log(data)
   
   return {
     props: {
-      
+      pokemons: data.results
     }
   }
 }
