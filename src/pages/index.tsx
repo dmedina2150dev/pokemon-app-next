66f940b2c7605f6e197440ea3f67456925f2ab22
Dajan Medina <dmedina2150dev@gmail.com>
2023-04-29 00:16:57 -0400

    [Add] interfaces para las respuesta de la API

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,9 @@ import { NextPage, GetStaticProps } from 'next';
 
 import { Layout } from '@/components/layouts';
 import { pokeApi } from '@/api';
+import { PokemonListResponse } from '@/interfaces';
+
+
 
 /**
  * 
@@ -38,7 +41,7 @@ const HomePage: NextPage = (props) => {
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
 
-  const { data } = await pokeApi.get('/pokemon?limit=151');
+  const { data } = await pokeApi.get<PokemonListResponse>('/pokemon?limit=151');
   // console.log(data)
   
   return {
