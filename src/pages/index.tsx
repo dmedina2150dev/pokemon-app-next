8d5c0b3e93436d7a4d03cadf3f624088ff12abc1
Dajan Medina <dmedina2150dev@gmail.com>
2023-04-28 20:23:40 -0400

    [Add] Files project [Add] Firts configurations [Add] Library NextUI

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import { Button } from '@nextui-org/react';
+import { NextPage } from 'next';
+
+
+const HomePage: NextPage = () => {
+  return (
+    <>
+      <h1>Hola Mundo</h1>
+      <Button color='gradient'>
+        Hola Mundo
+      </Button>
+    </>
+  )
+}
+
+export default HomePage;
