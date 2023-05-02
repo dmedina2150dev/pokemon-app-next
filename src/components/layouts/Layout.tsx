79ea686bae9abd95c0c8276771185e2f60a9d3b9
Dajan Medina <dmedina2150dev@gmail.com>
2023-05-02 19:46:40 -0400

    Haciendo CEO para compratir enlaces de mi web

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -1,15 +1,20 @@
 import { FC, PropsWithChildren } from 'react';
 import Head from 'next/head';
+
 import { Navbar } from '../ui';
 
 interface Props {
     title?: string
 }
+
+const origin = (typeof window === 'undefined') ? '' : window.location.origin;
 /**
  * ? PropsWithChildren -> Nos sirven para tipar con Typescript cuando el componente trae hijos los (children)
  * ? PropsWithChildren<Props> -> Le asignamos una interface o type que contenga el tipado de Typescript de nuestras (props)
  */
 export const Layout: FC<PropsWithChildren<Props>> = ({ children, title }) => {
+
+
     return (
         <>
             <Head>
@@ -17,6 +22,9 @@ export const Layout: FC<PropsWithChildren<Props>> = ({ children, title }) => {
                 <meta name="author" content="Dajan Medina" />
                 <meta name="description" content={`Información sobre el pokémon ${ title }`} />
                 <meta name="keywords" content={`${ title }, pokemon, pokedex`} />
+                <meta property="og:title" content={`Información sobre ${ title }`} />
+                <meta property="og:description" content={`Esta es la pagina de la información de ${ title }`} />
+                <meta property="og:image" content={`${ origin }/banner.png`} />
             </Head>
 
             {/* Navbar */}
